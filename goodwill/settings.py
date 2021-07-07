ecb7f9cc2ad157cc239caf6d506a9d25e349dd87
Jay Prakash <jayprakashsah9830@gmail.com>
2021-07-08 05:17:06 +0530

    Aws Important File Added

--- goodwill/settings.py
+++ goodwill/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT='587'
 EMAIL_HOST_USER='goodwillhometution@gmail.com'
-EMAIL_HOST_PASSWORD='Asdf@1234'
+EMAIL_HOST_PASSWORD='Goodwill@1234'
 EMAIL_USE_TLS=True
